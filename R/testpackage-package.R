8153b73153a65fd687a92a88010f4db8e5a2cee4
dcossyleon <dcossyleon@gmail.com>
2019-06-16 14:32:01 -0400

    Adds package documentation

---
+++ R/testpackage-package.R
@@ -0,0 +1,11 @@
+#' testpackage: A package to demonstrate R package utility for educators
+#' 
+#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
